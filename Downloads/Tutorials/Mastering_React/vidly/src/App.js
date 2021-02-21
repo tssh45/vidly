26e6ea24820aeead21541ebba0eabd60dd5f8b1c
tshamu <tshamu04@gmail.com>
2021-02-21 11:29:43 +0000

    Add routes

--- Downloads/Tutorials/Mastering_React/vidly/src/App.js
+++ Downloads/Tutorials/Mastering_React/vidly/src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
+import React, { Component } from 'react';
+import { Route, Link, Switch, Redirect } from 'react-router-dom'
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import MovieForm from './components/movieForm';
+import NavBar from './components/navBar';
+import LoginForm from './components/loginForm';
 
-function App() {
-  return (
-    <main className="container">
-      <Movies />
-    </main>
-  );
+class App extends Component {
+  
+  render() { 
+    return ( 
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+          <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+     );
+  }
 }
-
+ 
 export default App;
---
+++ Downloads/Tutorials/Mastering_React/vidly/src/components/loginForm.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Input from "./common/input";
+import Joi from "joi-browser";
+import Form from "./common/form";
+
+class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
+  doSubmit = () => {
+    console.log("Submitted");
+  };
+
+  render() {
+    const { data, errors } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
